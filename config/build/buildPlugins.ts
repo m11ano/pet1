d139b4bdf01866124a9bc1df98f7d51b2e7d670b
edkirill <edkirill@yandex.ru>
2023-09-06 18:28:43 +0600

    fix some bugs with webpack building

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -18,12 +18,12 @@ export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPlu
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
         }),
-        new BundleAnalyzerPlugin({
-            openAnalyzer: false,
-        }),
     ];
 
     if (isDev) {
+        plugins.push(new BundleAnalyzerPlugin({
+            openAnalyzer: false,
+        }));
         plugins.push(new ReactRefreshWebpackPlugin({ overlay: false }));
         plugins.push(new webpack.HotModuleReplacementPlugin());
     }
