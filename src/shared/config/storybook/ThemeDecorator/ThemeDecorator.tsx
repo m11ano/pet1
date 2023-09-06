dce023a7ae99174f142d26c431d0ab3bfb846749
edkirill <edkirill@yandex.ru>
2023-09-06 18:20:47 +0600

    add githum pipeline

--- src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
+++ src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
@@ -1,5 +1,4 @@
 import { StoryFn } from '@storybook/react';
-import 'app/styles/index.scss';
 import { Theme } from 'app/providers/ThemeProvider';
 
 export const ThemeDecorator = (theme: Theme) => (StoryComponent: StoryFn) => (
