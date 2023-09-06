b300a69a9df4c2db38be9244668f87b1d30e6de5
edkirill <edkirill@yandex.ru>
2023-09-06 18:07:00 +0600

    add loki for storybook

--- src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
+++ src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
@@ -1,6 +1,6 @@
 import { StoryFn } from '@storybook/react';
 import 'app/styles/index.scss';
-import { Theme, useTheme } from 'app/providers/ThemeProvider';
+import { Theme } from 'app/providers/ThemeProvider';
 
 export const ThemeDecorator = (theme: Theme) => (StoryComponent: StoryFn) => (
     <div className={`app ${theme}`}>
